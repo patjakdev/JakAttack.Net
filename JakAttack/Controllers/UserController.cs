6a86f3bfbb007af5317e865601c6f5d393b9c237
Patrick Jakubowski <pat@jakattack.net>
2011-11-28 22:59:28 -0800

    Adds PAPE extension to the OpenID request to force users to give their password on every login attempt

--- JakAttack/Controllers/UserController.cs
+++ JakAttack/Controllers/UserController.cs
@@ -8,6 +8,7 @@ using DotNetOpenAuth.OpenId;
 using DotNetOpenAuth.OpenId.RelyingParty;
 using DotNetOpenAuth.Messaging;
 using DotNetOpenAuth.OpenId.Extensions.AttributeExchange;
+using DotNetOpenAuth.OpenId.Extensions.ProviderAuthenticationPolicy;
 using System.Web.Security;
 using DotNetOpenAuth.OpenId.Extensions.OAuth;
 
@@ -32,12 +33,21 @@ namespace JakAttack.Controllers
                 if (Identifier.TryParse("jakattack.net", out id))
                 {
                     var req = s_relyingParty.CreateRequest(id);
+
+                    // Collect some information on the user with the authentication response
                     var fetch = new FetchRequest();
                     fetch.Attributes.Add(new AttributeRequest(WellKnownAttributes.Contact.Email, true));
                     fetch.Attributes.Add(new AttributeRequest(WellKnownAttributes.Name.First, true));
                     fetch.Attributes.Add(new AttributeRequest(WellKnownAttributes.Name.Last, true));
                     req.AddExtension(fetch);
 
+                    // Force the user to enter their password by setting the maximum authentication age to 0
+                    var pape = new PolicyRequest();
+                    pape.MaximumAuthenticationAge = new TimeSpan(0);
+                    req.AddExtension(pape);
+
+                    // Add a callback argument indicating where the login request came from. This is used to redirect the user when
+                    // the authentication response is received.
                     req.AddCallbackArguments("ReturnUrl", ReturnUrl);
 
                     return req.RedirectingResponse.AsActionResult();
@@ -48,8 +58,9 @@ namespace JakAttack.Controllers
                 switch (authResponse.Status)
                 {
                     case AuthenticationStatus.Authenticated:
-                        var fetch = authResponse.GetExtension<FetchResponse>();
                         FormsAuthentication.SetAuthCookie(authResponse.ClaimedIdentifier, false);
+
+                        var fetch = authResponse.GetExtension<FetchResponse>();
                         var user = new User 
                             { 
                                 ClaimedId   = authResponse.ClaimedIdentifier, 
